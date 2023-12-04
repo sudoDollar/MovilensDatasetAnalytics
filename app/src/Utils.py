d036445ddf2b49a6bb5965691e8eb67920b5d5ff
Ankit Rajvanshi <ankit131296@gmail.com>
2023-12-04 01:50:30 -0500

    Utils

---
+++ app/src/Utils.py
@@ -0,0 +1,5 @@
+class Utils:
+    MOVIE_FILE_PATH = "/Users/ankitrajvanshi/MS CE/Sem 3/Big Data/Project/MovilensDatasetAnalytics/Dataset/movies.dat"
+    RATINGS_FILE_PATH = "/Users/ankitrajvanshi/MS CE/Sem 3/Big Data/Project/MovilensDatasetAnalytics/Dataset/ratings.dat"
+    USERS_FILE_PATH = "/Users/ankitrajvanshi/MS CE/Sem 3/Big Data/Project/MovilensDatasetAnalytics/Dataset/users.dat"
+
